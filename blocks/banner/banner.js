672b006e912f4cd5829c4f2c1a8a91cf7e8c68b6
Akshay-Lachake <akshay.lachake@merkle.com>
2024-10-04 16:14:24 +0530

    icons added

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,21 +1,21 @@
-import { fetchPlaceholders } from '../../scripts/aem.js';
+import { fetchPlaceholders } from "../../scripts/aem.js";
 
 export default async function decorate(block) {
-const placeholders = await fetchPlaceholders();
+  const placeholders = await fetchPlaceholders();
 
-const { colors,clickMe } = placeholders;
+  const { colors, clickMe } = placeholders;
 
-console.log(colors)
-console.log(clickMe)
+  // console.log(colors)
+  // console.log(clickMe)
 
   block.classList.add("banner-body");
 
-  console.log(...block.children);
-  [...block.children].forEach((row,i) => {
-    row.classList.add("children-"+i++);
+  // console.log(...block.children);
+  [...block.children].forEach((row, i) => {
+    row.classList.add("children-" + i++);
 
     const collection = row.children;
     collection[0].classList.add("img-container");
     collection[1].classList.add("txt-container");
-  })
+  });
 }
