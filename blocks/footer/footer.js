267abbac8a3d7d6a15035dbd119ea0ed1a26aa3d
Akshay-Lachake <akshay.lachake@merkle.com>
2024-10-08 15:23:19 +0530

    footer changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,5 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
+import { getMetadata } from "../../scripts/aem.js";
+import { loadFragment } from "../fragment/fragment.js";
 
 /**
  * loads and decorates the footer
@@ -7,14 +7,22 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   // load footer as fragment
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const footerMeta = getMetadata("footer");
+  const footerPath = footerMeta
+    ? new URL(footerMeta, window.location).pathname
+    : "/footer";
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
-  block.textContent = '';
-  const footer = document.createElement('div');
+  block.textContent = "";
+  const footer = document.createElement("div");
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+  document.querySelectorAll(":scope .footer-nav ul li a").forEach((anchor) => {
+    if (window.location.href.indexOf(anchor.getAttribute("href")) > -1) {
+      anchor.classList.add("active");
+    }
+  });
 }
